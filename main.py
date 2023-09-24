950557057b94caaa94607edd911bb84420656f91
Isaiah Nields <imnields@gmail.com>
2023-09-24 08:47:25 -0700

    add video output

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 from datetime import datetime, timezone
 from moviepy import *
+from moviepy.editor import *
 
 
 parser = argparse.ArgumentParser(
@@ -11,6 +12,18 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("--gpx-file", help="GPX file of ride", required=True)
 parser.add_argument("--video-file", help="Video file of ride", required=True)
+parser.add_argument(
+    "--output-start",
+    help="Starting time of output video in seconds",
+    default=0,
+    type=int,
+)
+parser.add_argument(
+    "--output-length",
+    help="Length of video output in seconds",
+    default=None,
+    type=int,
+)
 args = vars(parser.parse_args())
 
 
@@ -20,7 +33,7 @@ def load_gpx_file():
     return gpx
 
 
-def get_data_from_gpx(gpx):
+def get_coordinates_from_gpx(gpx):
     coordinates = []
     for track in gpx.tracks:
         for segment in track.segments:
@@ -35,6 +48,7 @@ def get_data_from_gpx(gpx):
                     }
                 )
     coordinates = add_speed_to_coordinates(coordinates)
+    coordinates = add_duration_to_coordinates(coordinates)
     coordinates = add_extension_data_to_coordinates(coordinates)
     return coordinates
 
@@ -45,17 +59,24 @@ def add_speed_to_coordinates(coordinates):
     for coordinate in coordinates:
         coordinates_with_speed.append(
             {
-                "latitude": coordinate["latitude"] - first["latitude"],
-                "longitude": coordinate["longitude"] - first["longitude"],
+                **coordinate,
                 "speed": 0.0
                 if len(coordinates_with_speed) == 0
                 else coordinate["point"].speed_between(
                     coordinates_with_speed[-1]["point"]
                 ),
-                **coordinate,
             }
         )
     return coordinates_with_speed
+
+
+def add_duration_to_coordinates(coordinates):
+    for idx, pair in enumerate(zip(coordinates, coordinates[1:])):
+        curr, next = pair
+        coordinates[idx]["duration"] = next["time"] - curr["time"]
+
+    coordinates[-1]["duration"] = 1
+    return coordinates
 
 
 def add_extension_data_to_coordinates(coordinates):
@@ -87,21 +108,92 @@ def fetch_mp4_exif_data():
     return dict(out)
 
 
-def get_video_start_timestamp(exif_data):
+def get_video_start_and_end_timestamp(exif_data):
     video_start_time = datetime.strptime(
         exif_data["Track Create Date"], "%Y:%m:%d %H:%M:%S"
     )
+    hours, minutes, seconds = [int(i) for i in exif_data["Track Duration"].split(":")]
     video_start_time = video_start_time.replace(tzinfo=timezone.utc)
-    return datetime.timestamp(video_start_time)
+    start_timestamp = datetime.timestamp(video_start_time)
+    return start_timestamp, start_timestamp + (hours * 3600) + (minutes * 60) + seconds
+
+
+def adjust_start_and_end_timestamp(start, end):
+    adjusted_start_timestamp = start + args["output_start"]
+
+    if args["output_length"] is not None:
+        adjusted_end_timestamp = adjusted_start_timestamp + args["output_length"]
+    else:
+        adjusted_end_timestamp = end
+
+    return adjusted_start_timestamp, adjusted_end_timestamp
+
+
+def get_video_time_range(start_timestamp, end_timestamp):
+    subclip_start = args["output_start"]
+    if args["output_length"] is None:
+        subclip_end = (end_timestamp - start_timestamp) - args["output_start"]
+    else:
+        subclip_end = args["output_start"] + (
+            args["output_length"] if args["output_length"] is not None else 0
+        )
+    return subclip_start, subclip_end
+
+
+def filter_coordinates_and_fix_edge_coordinates(
+    coordinates, adjusted_start_timestamp, adjusted_end_timestamp
+):
+    filtered_coordinates = []
+    for coordinate in coordinates:
+        if adjusted_end_timestamp >= coordinate["time"] >= adjusted_start_timestamp:
+            filtered_coordinates.append(coordinate)
+        elif True:
+            pass
+
+    return filtered_coordinates
 
 
 if __name__ == "__main__":
-    data = fetch_mp4_exif_data()
-    start_timestamp = get_video_start_timestamp(data)
-    print(start_timestamp)
+    exif_data = fetch_mp4_exif_data()
+    start_timestamp, end_timestamp = get_video_start_and_end_timestamp(exif_data)
 
-    gpx = load_gpx_file()
-    data = [
-        datum for datum in get_data_from_gpx(gpx) if datum["time"] > start_timestamp
-    ]
-    print(len(data))
+    adjusted_start_timestamp, adjusted_end_timestamp = adjust_start_and_end_timestamp(
+        start_timestamp, end_timestamp
+    )
+
+    gpx_data = load_gpx_file()
+
+    coordinates = get_coordinates_from_gpx(gpx_data)
+
+    print(
+        f"Found {len(coordinates)} coordinates between times {adjusted_start_timestamp} "
+        f"and {adjusted_end_timestamp} ({adjusted_end_timestamp - adjusted_start_timestamp} seconds)"
+    )
+
+    subclip_range = get_video_time_range(
+        start_timestamp,
+        end_timestamp,
+    )
+
+    print(
+        f"Clipping {subclip_range[1] - subclip_range[0]} seconds of video from {subclip_range[0]} "
+        f"to {subclip_range[1]}"
+    )
+
+    clip = VideoFileClip(args["video_file"]).subclip(*subclip_range)
+
+    text_clip = concatenate_videoclips(
+        [
+            TextClip(str(coordinate["time"]), fontsize=70, color="white")
+            .set_pos(("right", "top"))
+            .set_duration(coordinate["duration"])
+            for coordinate in coordinates
+        ]
+    ).subclip(0, subclip_range[1] - subclip_range[0])
+
+    for coordinate in coordinates:
+        print(coordinate["duration"])
+
+    video = CompositeVideoClip([clip, text_clip])
+
+    video.write_videofile("out.mp4")
