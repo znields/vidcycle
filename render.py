679084865fb26c411a43944c8c5247b211d6bba4
Isaiah Nields <imnields@gmail.com>
2023-10-10 01:17:07 +0000

    optimize render speed

--- render.py
+++ render.py
@@ -8,11 +8,13 @@ import numpy as np
 import matplotlib.patches as patches
 from matplotlib.path import Path
 from typing import Optional
-from typing import Any
+from typing import Any, Tuple, List
 import os
 
 FRAMES_PER_SECOND = 30
-DEFAULT_FONT_SIZE = 190
+STAT_FONT_SIZE = 350
+LABEL_FONT_SIZE = 100
+STAT_CLIP_X_POS = 0.02
 
 
 def write_video(
@@ -47,49 +49,41 @@ def write_video(
     MAP_AND_LOCATION_POSITION = (0.01, 0.05)
     LOCATION_POINT_SIZE = 15 * scale_factor
 
-    stat_clips = get_stat_clips(
+    stat_clips, label_clips = get_stat_clips(
         garmin_subsegment, video_length, stats_refresh_period, scale_factor
     )
 
+    map_clip, location_clip_inner, location_clip_outer = get_map_clips(
+        garmin_segment,
+        garmin_start_time,
+        video_length,
+        stats_refresh_period,
+        LOCATION_POINT_SIZE,
+        LOCATION_POINT_SIZE * 2,
+    )
+
     map_clip = (
-        get_map_clip(
-            garmin_segment,
-            video_length,
-            stats_refresh_period,
-        )
-        .set_opacity(0.75)
+        map_clip.set_opacity(0.75)
         .set_position(MAP_AND_LOCATION_POSITION, relative=True)
         .resize(MAP_AND_LOCATION_CLIP_SIZE)
     )
 
     location_clip_inner = (
-        get_location_clip(
-            garmin_segment,
-            video_length,
-            garmin_start_time,
-            stats_refresh_period,
-            LOCATION_POINT_SIZE,
-        )
-        .set_opacity(0.75)
+        location_clip_inner.set_opacity(0.75)
         .set_position(MAP_AND_LOCATION_POSITION, relative=True)
         .resize(MAP_AND_LOCATION_CLIP_SIZE)
     )
 
     location_clip_outer = (
-        get_location_clip(
-            garmin_segment,
-            video_length,
-            garmin_start_time,
-            stats_refresh_period,
-            LOCATION_POINT_SIZE * 2,
-        )
-        .set_opacity(0.3)
+        location_clip_outer.set_opacity(0.3)
         .set_position(MAP_AND_LOCATION_POSITION, relative=True)
         .resize(MAP_AND_LOCATION_CLIP_SIZE)
     )
 
     video = CompositeVideoClip(
-        [clip, map_clip, location_clip_inner, location_clip_outer] + stat_clips
+        [clip, map_clip, location_clip_inner, location_clip_outer]
+        + stat_clips
+        + label_clips
     )
 
     if out_video_path is None:
@@ -103,7 +97,7 @@ def get_stat_clips(
     video_length: timedelta,
     stats_refresh_period: timedelta,
     scale_factor: float,
-):
+) -> Tuple[List[VideoClip], List[TextClip]]:
     def data_to_str(data: Any):
         if data is None:
             return "0"
@@ -114,6 +108,7 @@ def get_stat_clips(
         return str(data)
 
     stat_clips = []
+    label_clips = []
     for idx, key_and_label in enumerate(
         [
             ("speed", "MPH"),
@@ -127,9 +122,8 @@ def get_stat_clips(
         for coordinate in garmin_segment.get_iterator(stats_refresh_period):
             text_clip = (
                 TextClip(
-                    data_to_str(coordinate.__dict__[key])
-                    + ("" if label is None else "\n" + label),
-                    fontsize=DEFAULT_FONT_SIZE * scale_factor,
+                    data_to_str(coordinate.__dict__[key]),
+                    fontsize=STAT_FONT_SIZE * scale_factor,
                     color="white",
                     font="Helvetica-Bold",
                 )
@@ -140,122 +134,116 @@ def get_stat_clips(
 
         stat_clip = (
             concatenate_videoclips(text_clips)
-            .set_position((0.01, idx / 6), relative=True)
+            .set_position((STAT_CLIP_X_POS, idx / 6), relative=True)
             .subclip(0, video_length.total_seconds())
         )
 
         stat_clips.append(stat_clip)
-    return stat_clips
+
+        label_clips.append(
+            TextClip(
+                label,
+                fontsize=LABEL_FONT_SIZE * scale_factor,
+                color="white",
+                font="Helvetica-Bold",
+            )
+            .set_duration(stats_refresh_period.total_seconds() * len(text_clips))
+            .set_position((STAT_CLIP_X_POS, (idx / 6) - 0.015), relative=True)
+            .set_opacity(0.75),
+        )
+    return stat_clips, label_clips
 
 
-# TODO: add option to render only part of map
-def get_map_clip(
+def get_map_clips(
     garmin_segment: GarminSegment,
+    garmin_start_time: datetime,
     video_length: timedelta,
     stats_refresh_period: timedelta,
-):
-    fig_mpl = plt.figure(frameon=False, facecolor="black")
-    ax = fig_mpl.add_axes([0, 0, 1, 1])
-    ax.set_facecolor("black")
-
+    inner_marker_size: int,
+    outer_marker_size: int,
+) -> Tuple[ImageClip, VideoClip, VideoClip]:
     verts = [
         (c.longitude, c.latitude)
         for c in garmin_segment.get_iterator(stats_refresh_period)
     ]
-    codes = [Path.MOVETO] + [Path.CURVE3 for _ in range(len(verts) - 1)]
-    path = Path(verts, codes)
-    patch = patches.PathPatch(path, edgecolor="white", facecolor="none", lw=6)
-    ax.add_patch(patch)
-
     xs = [vert[0] for vert in verts]
     ys = [vert[1] for vert in verts]
     min_x, max_x = min(xs), max(xs)
     min_y, max_y = min(ys), max(ys)
     dx, dy = max_x - min_x, max_y - min_y
-    ax.set_xlim(min_x - (dx * 0.01), max_x + (dx * 0.01))
-    ax.set_ylim(min_y - (dy * 0.01), max_y + (dy * 0.01))
 
-    def make_frame_mpl(t):
-        t = timedelta(seconds=t)
+    def get_segment_clip():
+        fig_mpl = plt.figure(frameon=False, facecolor="black")
+        ax = fig_mpl.add_axes([0, 0, 1, 1])
+        ax.set_facecolor("black")
+
+        codes = [Path.MOVETO] + [Path.CURVE3 for _ in range(len(verts) - 1)]
+        path = Path(verts, codes)
+        patch = patches.PathPatch(path, edgecolor="white", facecolor="none", lw=6)
+        ax.add_patch(patch)
+
+        ax.set_xlim(min_x - (dx * 0.01), max_x + (dx * 0.01))
+        ax.set_ylim(min_y - (dy * 0.01), max_y + (dy * 0.01))
 
         frame = mplfig_to_npimage(fig_mpl)
-
-        return frame
-
-    def make_mask_mpl(t):
-        t = timedelta(seconds=t)
-
-        frame = make_frame_mpl(t.total_seconds())
         x, y, _ = frame.shape
         mask = (np.sum(frame, axis=2) > 10).reshape(x, y)
-        return mask
 
-    mask = VideoClip(make_mask_mpl, duration=video_length.total_seconds(), ismask=True)
-    clip = VideoClip(make_frame_mpl, duration=video_length.total_seconds())
+        mask = ImageClip(mask, duration=video_length.total_seconds(), ismask=True)
+        clip = ImageClip(frame, duration=video_length.total_seconds())
 
-    return clip.set_mask(mask)
+        return clip.set_mask(mask)
 
+    def get_location_marker_clip(marker_size: int):
+        fig_mpl = plt.figure(frameon=False, facecolor="black")
+        ax = fig_mpl.add_axes([0, 0, 1, 1])
+        ax.set_facecolor("black")
 
-def get_location_clip(
-    garmin_segment: GarminSegment,
-    video_length: timedelta,
-    garmin_start_time: datetime,
-    stats_refresh_period: timedelta,
-    marker_size: int,
-):
-    fig_mpl = plt.figure(frameon=False, facecolor="black")
-    ax = fig_mpl.add_axes([0, 0, 1, 1])
-    ax.set_facecolor("black")
+        (marker,) = ax.plot(
+            [verts[0][0]],
+            [verts[0][1]],
+            marker="o",
+            markersize=marker_size,
+            markerfacecolor="white",
+            markeredgecolor="white",
+        )
 
-    verts = [
-        (c.longitude, c.latitude)
-        for c in garmin_segment.get_iterator(stats_refresh_period)
-    ]
+        ax.set_xlim(min_x - (dx * 0.01), max_x + (dx * 0.01))
+        ax.set_ylim(min_y - (dy * 0.01), max_y + (dy * 0.01))
 
-    (marker,) = ax.plot(
-        [verts[0][0]],
-        [verts[0][1]],
-        marker="o",
-        markersize=marker_size,
-        markerfacecolor="white",
-        markeredgecolor="white",
+        def make_frame_mpl(t):
+            t = timedelta(seconds=t)
+            coordinate = garmin_segment.get_coordinate(garmin_start_time + t)
+
+            marker.set_xdata([coordinate.longitude])
+            marker.set_ydata([coordinate.latitude])
+
+            return mplfig_to_npimage(fig_mpl)
+
+        def make_mask_mpl(t):
+            t = timedelta(seconds=t)
+            coordinate = garmin_segment.get_coordinate(garmin_start_time + t)
+
+            marker.set_xdata([coordinate.longitude])
+            marker.set_ydata([coordinate.latitude])
+
+            frame = make_frame_mpl(t.total_seconds())
+            x, y, _ = frame.shape
+            mask = (np.sum(frame, axis=2) > 10).reshape(x, y, 1)
+            return mask
+
+        mask = VideoClip(
+            make_mask_mpl, duration=video_length.total_seconds(), ismask=True
+        )
+        clip = VideoClip(make_frame_mpl, duration=video_length.total_seconds())
+
+        return clip.set_mask(mask)
+
+    return (
+        get_segment_clip(),
+        get_location_marker_clip(inner_marker_size),
+        get_location_marker_clip(outer_marker_size),
     )
-
-    xs = [vert[0] for vert in verts]
-    ys = [vert[1] for vert in verts]
-    min_x, max_x = min(xs), max(xs)
-    min_y, max_y = min(ys), max(ys)
-    dx, dy = max_x - min_x, max_y - min_y
-    ax.set_xlim(min_x - (dx * 0.01), max_x + (dx * 0.01))
-    ax.set_ylim(min_y - (dy * 0.01), max_y + (dy * 0.01))
-
-    def make_frame_mpl(t):
-        t = timedelta(seconds=t)
-        coordinate = garmin_segment.get_coordinate(garmin_start_time + t)
-
-        marker.set_xdata([coordinate.longitude])
-        marker.set_ydata([coordinate.latitude])
-
-        frame = mplfig_to_npimage(fig_mpl)
-
-        return frame
-
-    def make_mask_mpl(t):
-        t = timedelta(seconds=t)
-        coordinate = garmin_segment.get_coordinate(garmin_start_time + t)
-
-        marker.set_xdata([coordinate.longitude])
-        marker.set_ydata([coordinate.latitude])
-
-        frame = make_frame_mpl(t.total_seconds())
-        x, y, _ = frame.shape
-        mask = (np.sum(frame, axis=2) > 10).reshape(x, y, 1)
-        return mask
-
-    mask = VideoClip(make_mask_mpl, duration=video_length.total_seconds(), ismask=True)
-    clip = VideoClip(make_frame_mpl, duration=video_length.total_seconds())
-
-    return clip.set_mask(mask)
 
 
 def write_optimized_video(in_video_path: str, optimized_video_resolution: int) -> str:
@@ -266,5 +254,5 @@ def write_optimized_video(in_video_path: str, optimized_video_resolution: int) -
         return path
 
     clip = VideoFileClip(in_video_path).resize(height=optimized_video_resolution)
-    clip.write_videofile(path)
+    clip.write_videofile(path, threads=64)
     return path
